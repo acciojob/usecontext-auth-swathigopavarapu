b15af13fe0abf37b05882d195dc109a381ec7233
Swathi Gopavarapu <swathigopavarapu1@gmail.com>
2025-09-24 10:31:33 +0530

    initial commit - useContext auth

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,54 @@
 
-import React from "react";
+import React,{ createContext, useContext, useState } from "react";
 import './../styles/App.css';
+
+const AuthContext = createContext();
+
+
+const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const toggleAuth = () => {
+    setIsAuthenticated((prev) => !prev);
+  };
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, toggleAuth }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+const Auth = () => {
+  const { isAuthenticated, toggleAuth } = useContext(AuthContext);
+
+  return (
+    <div className="auth-container">
+      <label>
+        <input
+          type="checkbox"
+          checked={isAuthenticated}
+          onChange={toggleAuth}
+        />{" "}
+        I am not a robot
+      </label>
+
+      <p>
+        {isAuthenticated
+          ? "You are authenticated!"
+          : "You are not authenticated."}
+      </p>
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <AuthProvider>
+        <h1>useContext Authentication Demo</h1>
+        <Auth />
+      </AuthProvider>
     </div>
   )
 }
